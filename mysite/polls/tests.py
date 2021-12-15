2b9bded3074d5b712504cdbec6a6d21cfde80207
andrei-barushka <andrei.barushka@gmail.com>
2021-12-15 12:35:50 +0300

    Fix one test case.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -62,7 +62,7 @@ class QuestionIndexViewTests(TestCase):
         Questions with a pub_date in the past are displayed on the
         index page.
         """
-        questions = create_question(question_text="Past questions.", days=30)
+        questions = create_question(question_text="Past questions.", days=-30)
         response = self.client.get(reverse('polls:index'))
         self.assertQuerysetEqual(
             response.context['latest_question_list'],
